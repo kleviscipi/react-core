24fa03c778ebbcca01b3fe00c6f835b872b42025
Klevis Cipi <cipiklevis@gmail.com>
2018-08-03 18:05:19 +0200

    Build New Structure React Start Bolerplate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Root from './core/route/index.js';
-import createStore from './core/store/configureStore.js'
+import Root from './app/core/route/index.js';
+import createStore from './app/core/store/configureStore.js'
+import environmentProd  from './environment/environment.prod.js'
+import environmentLocal from './environment/environment.js'
+/***INCLUDE BOOTSTRAP***/
+
+import './assets/css/style.css'
+import 'bootstrap';
+
+window.environment = process.env=="local"?environmentLocal:environmentProd
 
 const initialState = {}
 const store = createStore(initialState)
+
+
 
 let render = () => {
 	ReactDOM.render(<Root store={store} />,
